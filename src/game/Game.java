69bafd8c9cb10a348d193282f15d777248540ae6
Lucas Sindra <lucassindra@gmail.com>
2017-12-22 17:49:12 -0200

    First commit, game already has a game loop and window
    Signed-off-by: Lucas Sindra <lucassindra@gmail.com>

---
+++ src/game/Game.java
@@ -0,0 +1,126 @@
+package game;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+public class Game {
+
+	public final static int WIDTH = 800;
+	public final static int HEIGHT = 600;
+
+	private String gameName = "Flappy Bird";
+
+	private Canvas game = new Canvas();
+
+	private Input input;
+
+	private ArrayList<Updatable> updatableObjects = new ArrayList<>();
+	private ArrayList<Renderable> renderablesObjects = new ArrayList<>();
+
+	public void addUpdatable(Updatable updatable) {
+		updatableObjects.add(updatable);
+	}
+
+	public void removeUpdatable(Updatable updatable) {
+		updatableObjects.remove(updatable);
+	}
+
+	public void addRenderable(Renderable renderable) {
+		renderablesObjects.remove(renderable);
+	}
+
+	public void removeRenderable(Renderable renderable) {
+		renderablesObjects.remove(renderable);
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.renderablesObjects.add(new Renderable() {
+			@Override
+			public void render(Graphics2D graphics, float interpolation) {
+				graphics.setColor(Color.RED);
+				graphics.drawRect(300, 250, 50, 100);
+			}
+		});
+		game.start();
+	}
+
+	public void start() {
+		// Init window
+		initWindow();
+
+		// Init input
+		input = new Input();
+
+		// Init game loop
+		final int TICKS_PER_SECOND = 60;
+		final int TIME_PER_TICK = 1000 / TICKS_PER_SECOND;
+		final int MAX_FRAMESKIPS = 5;
+
+		long nextGameTick = System.currentTimeMillis();
+		int loops;
+		float interpolation;
+
+		long timeAtLastFPSCheck = 0;
+		int ticks = 0;
+
+		boolean running = true;
+
+		while (running) {
+			// Updating
+
+			// Rendering
+
+			// FPS Check
+		}
+
+		// Game end
+	}
+
+	private void initWindow() {
+		Dimension gameSize = new Dimension(Game.WIDTH, Game.HEIGHT);
+		JFrame gameWindow = new JFrame(gameName);
+
+		gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameWindow.setSize(gameSize);
+		gameWindow.setResizable(false);
+		gameWindow.setVisible(true);
+
+		game.setSize(gameSize);
+		game.setMinimumSize(gameSize);
+		game.setMaximumSize(gameSize);
+		game.setPreferredSize(gameSize);
+
+		gameWindow.add(game);
+		gameWindow.setLocationRelativeTo(null);
+	}
+
+	private void updateObjects() {
+		for (Updatable updatable : updatableObjects) {
+			updatable.update(input);
+		}
+	}
+
+	private void renderObjects(float interpolation) {
+		BufferStrategy bufferStrategy = game.getBufferStrategy();
+		if (bufferStrategy == null) {
+			game.createBufferStrategy(2);
+			return;
+		}
+
+		Graphics2D graphics = (Graphics2D) bufferStrategy.getDrawGraphics();
+		graphics.clearRect(0, 0, game.getWidth(), game.getHeight());
+
+		for (Renderable renderable : renderablesObjects) {
+			renderable.render(graphics, interpolation);
+		}
+
+		graphics.dispose();
+		bufferStrategy.show();
+	}
+}
---
+++ src/game/Updatable.java
@@ -0,0 +1,5 @@
+package game;
+
+public interface Updatable {
+	public void update(Input input);
+}
